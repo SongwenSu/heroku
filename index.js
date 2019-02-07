e05a14f94015c45f2997d03b65ae42ce9da43667
SongwenSu <swsu@ucdavis.edu>
2019-02-06 22:09:13 -0800

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ app.use(bodyParser.json());
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.get('/', (req, res) => res.render('pages/index'))
-app.post('/form-submitted', (req, res) => res.render('pages/page_submitted'))
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 });
 
