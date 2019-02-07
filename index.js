e306f19b6e1428d8f89272cd0ddb2e100b237141
SongwenSu <swsu@ucdavis.edu>
2019-02-06 21:43:22 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+var app = express();
+var myParser = require('body-parser')
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .post('/form-submitted', function(req, res){console.log(req.body)})
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+app.use(myParser.urlencoded({extended : true}));
+app.post('/page-submitted', function(req, res) {
+  console.log(req.body) 
+}
